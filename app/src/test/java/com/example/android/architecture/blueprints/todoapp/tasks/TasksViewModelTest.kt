2653e05dca4d4fd34c53ae6d1a6131c0af04c3aa
David Rawson <david.rawson@coretex.com>
2020-11-23 08:10:26 +1300

    Update AGP

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -32,7 +32,6 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-
 /**
  * Unit tests for the implementation of [TasksViewModel]
  */
